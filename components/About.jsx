2644b42cda2c5df683c1636940f20c3214d31392
julian parera <julian.parera@hotmail.com>
2023-12-20 17:35:01 -0300

    Arreglo del about

--- components/About.jsx
+++ components/About.jsx
@@ -17,11 +17,8 @@ const About = () => {
           I am Julian Ignacio Parera, a 27-year-old. Since my early 20s, I have been looking for something that makes me feel passionate and excited. I have tried studying several things, and in 2018, I started pursuing a B.A. in Audiovisual Arts at a university in Argentina, expecting to graduate in 2024. In the same year, I will also begin my studies in a university technical program in Computer Systems.
         </p>
         <br />
-        <p className="text-xl">
+        <p className="text-xl mb-10">
           During the COVID-19 pandemic, I discovered how useful and interesting Web Development is. I found its possibilities and capabilities to be fascinating. So it was an instant decision; I wanted to study Web Development. Ever since then, I have been studying and attending courses from digital academies as well as various courses on platforms like Udemy and YouTube. Throughout this time, I rapidly progressed from knowing nothing to being capable of building and coding different Web Apps, such as a Loan Website Simulator, a Cryptocurrencies Price Web, a To-do list, and more. Now, I believe I am ready to take my first steps in the IT sector and pursue a fulfilling career in Web Development.
-
-          Portfolio
-          Check out some of my work here:
         </p>
       </div>
     </div>
